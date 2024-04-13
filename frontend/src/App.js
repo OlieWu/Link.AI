fe4a65878e32d2a860a4c1c3330e9479cae296ad
ruiqixu0826 <139551394+ruiqixu0826@users.noreply.github.com>
2024-04-13 21:18:50 +0000

    update step2&3, complete path logics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Search from './components/search/search';
-import Spotify from './components/spotify/spotify';
 import Result from './components/result/result';
 
 function App() {
