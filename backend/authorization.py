ffaa11009f997586ae4243c9d773abee0e65b251
OlieWu <oliverwu@umich.edu>
2024-04-13 22:47:01 -0400

    login final

--- backend/authorization.py
+++ backend/authorization.py
@@ -1,86 +1,56 @@
 from dotenv import load_dotenv
 import os
-import secrets
-import asyncio
-import hashlib
-import base64
-import flask
-import string
-import random
-from urllib.parse import urlencode
+from flask import Flask, jsonify, request, redirect
+from flask_cors import CORS
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import Flask, jsonify
-from flask_cors import CORS
- 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
+
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000'], methods=['GET', 'POST'], allowed_headers=['Content-Type', 'Authorization'])
+CORS(app, origins=['http://localhost:3000'], methods=['GET',
+     'POST'], allowed_headers=['Content-Type', 'Authorization'])
 app.config['CORS_HEADERS'] = 'Content-Type'
-SPOTIFY_REDIRECT_URI = "http://localhost:5000/callback" # TODO: update to our main app page
+
+load_dotenv()
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = "http://localhost:5000/callback"
+SPOTIFY_SCOPES = ["user-library-read", "user-top-read", "user-read-recently-played",
+                  "playlist-read-private", "playlist-read-collaborative", "user-read-private", "user-read-email"]
+
 global_data = {
-    "sp_oauth": None,
-    "sp": None,
-    "auth_url": None,
+    "sp_oauth": SpotifyOAuth(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        redirect_uri=SPOTIFY_REDIRECT_URI,
+        scope=SPOTIFY_SCOPES
+    )
 }
 
-def generate_random_string(length):
-  """Generates a random string of the specified length."""
-  chars = string.ascii_letters + string.digits
-  return ''.join(random.choice(chars) for _ in range(length))
 
 @app.route('/login', methods=['GET'])
 def login():
-    load_dotenv()
-    SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-    SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-    SPOTIFY_SCOPES = ["user-top-read", "user-read-recently-played", "playlist-read-private", "playlist-read-collaborative", "user-read-private", "user-read-email"]
-    
-    global_data["sp_oauth"] = SpotifyOAuth(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI, scope=SPOTIFY_SCOPES)
-    global_data["auth_url"] = global_data["sp_oauth"].get_authorize_url()
-
-    return jsonify({"url": global_data["auth_url"]})
-
+    auth_url = global_data["sp_oauth"].get_authorize_url()
+    return jsonify({"url": auth_url})
 
 
 @app.route('/callback', methods=['GET'])
 def callback():
-    # auth_url = global_data["sp_oauth"].get_authorize_url()
-    code = global_data["sp_oauth"].parse_response_code(global_data["auth_url"])
-
+    code = request.args.get('code')
     token_info = global_data["sp_oauth"].get_access_token(code)
-    access_token = token_info['access_token']
-
-    global_data["sp"] = spotipy.Spotify(auth=access_token)
-
-    # TODO: do database action here
-    print("hungry")
-
-    return flask.redirect("/")
+    global_data['sp'] = spotipy.Spotify(auth=token_info['access_token'])
+    return redirect('http://localhost:3000')
 
 
-# def get_token():
-#     load_dotenv()
-#     client_id = os.getenv("CLIENT_ID")
-#     client_secret = os.getenv("CLIENT_SECRET")
-#     auth_string = client_id + ":" + client_secret
-#     auth_bytes = auth_string.encode("utf-8")
-#     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-#     url = "https://accounts.spotify.com/api/token"
-#     headers = {
-#         "Authorization": "Basic " + auth_base64,
-#         "Content-Type": "application/x-www-form-urlencoded"
-#     }
-
-#     data = {"grant_type": "client_credentials"}
-#     result = post(url, headers=headers, data=data)
-#     json_result = json.loads(result.content)
-#     token = json_result["access_token"]
-
-#     return token
+@app.route('/get-liked-songs', methods=['GET'])
+def get_liked_songs():
+    if 'sp' in global_data:
+        tracks = global_data['sp'].current_user_saved_tracks()
+        # Extracting a simple list of track names and artists for example
+        track_list = [{'name': item['track']['name'], 'artist': item['track']
+                       ['artists'][0]['name']} for item in tracks['items']]
+        return jsonify(track_list)
+    return jsonify({'error': 'Spotify client not initialized'}), 500
 
 
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+if __name__ == '__main__':
+    app.run(debug=True)
