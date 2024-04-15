8a541e02535b266a6290427873591b59a1042ce3
OlieWu <oliverwu@umich.edu>
2024-04-15 13:54:29 -0400

    remove key

--- searchify/src/firebase.js
+++ searchify/src/firebase.js
@@ -6,11 +6,6 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "searchify-cbc7b.firebaseapp.com",
-  projectId: "searchify-cbc7b",
-  storageBucket: "searchify-cbc7b.appspot.com",
-  messagingSenderId: "984307538973",
-  appId: "1:984307538973:web:1770814d6849f0a3883af4",
 };
 
 // Initialize Firebase
